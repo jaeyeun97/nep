774768f8aa2a9eeaa44d31245517bc6a5401fd07
Nathan Corbyn <me@nathancorbyn.com>
2018-07-17 22:25:17 +0100

    Add minute dbus example

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,8 +1,16 @@
+extern crate dbus;
+
+use dbus::{BusType, Connection, Message};
+
 mod terminal;
 
 use terminal::Terminal;
 
 fn main() {
+    let c = Connection::get_private(BusType::Session).unwrap();
+    let m = Message::new_method_call("org.nep", "/hello", "org.nep", "Hello").unwrap();
+    c.send(m).unwrap();
+
     {
         let mut term = Terminal::new();
         // Initialisation and setup would be done here
--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -1,3 +1,32 @@
+extern crate dbus;
+
+use dbus::tree::Factory;
+use dbus::{BusType, Connection, NameFlag};
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, World");
+    let c = Connection::get_private(BusType::Session).unwrap();
+    c.register_name("org.nep", NameFlag::ReplaceExisting as u32)
+        .unwrap();
+
+    let f = Factory::new_fn::<()>();
+
+    let tree = f.tree(()).add(
+        f.object_path("/hello", ())
+            .introspectable()
+            .add(
+                f.interface("org.nep", ())
+                    .add_m(f.method("Hello", (), move |m| {
+                        println!("Hello!");
+                        Ok(vec![])
+                    })),
+            ),
+    );
+
+    tree.set_registered(&c, true).unwrap();
+    c.add_handler(tree);
+
+    loop {
+        c.incoming(1000).next();
+    }
 }
